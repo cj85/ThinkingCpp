45949e156a878ec57e078edca7857ac5b6e16d06
Chen Ji <chen.ji@duke.edu>
2013-09-24 08:53:08 +0800

    Initial Commit

---
+++ ThinkingCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ThinkingCpp
+//
+//  Created by jichen on 13-9-24.
+//  Copyright (c) 2013年 jichen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
